5323d5e6daba0550f023ea85ab14ecdeb6454942
AeksandrGavrilov <GavrilovAleksandr20011994@yandex.ru>
2025-09-25 22:09:49 +0400

    обновление файлов конфигураций

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { DialogService } from 'primeng/dynamicdialog'
+import { DialogModule } from 'primeng/dialog';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +16,8 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    DialogService,
+    importProvidersFrom(DialogModule),
     providePrimeNG({
       theme: {
         preset: Aura
