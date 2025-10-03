4cb00531dd4b40fa94f00e0b5a1620dfd2f1c9bb
AeksandrGavrilov <GavrilovAleksandr20011994@yandex.ru>
2025-10-03 11:04:40 +0400

    созданы заглушки для будущих компонентов

---
+++ src/app/shared/components/advert-details/advert-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advert-details',
+  imports: [],
+  templateUrl: './advert-details.component.html',
+  styleUrl: './advert-details.component.scss',
+  standalone: true,
+})
+export class AdvertDetailsComponent {
+
+}
---
+++ src/app/shared/components/my-adverts/my-adverts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-adverts',
+  imports: [],
+  templateUrl: './my-adverts.component.html',
+  styleUrl: './my-adverts.component.scss',
+  standalone: true,
+})
+export class MyAdvertsComponent {
+
+}
