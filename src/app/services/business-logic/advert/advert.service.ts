5d29ce56aaef801b07e4b70e9aa386108b47fd25
AeksandrGavrilov <GavrilovAleksandr20011994@yandex.ru>
2025-10-07 16:11:52 +0400

    создание новых методов для получения категорий и создания объявлений

--- src/app/services/business-logic/advert/advert.service.ts
+++ src/app/services/business-logic/advert/advert.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { AdvertApiService } from '../../api/advert-api/advert-api.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { SearchAdvertsRequest } from '../../../interfaces/adverts-request.interface';
 import { Advert } from '../../../interfaces/advert.interface';
 
@@ -10,9 +10,30 @@ import { Advert } from '../../../interfaces/advert.interface';
 })
 export class AdvertService {
   private advertApiService = inject(AdvertApiService);
+  private currentCategory = new BehaviorSubject<string | null>(null);
 
   searchAdvert(request: SearchAdvertsRequest): Observable<Advert[]> {
     return this.advertApiService.searchAdverts(request)
-  }
+  };
   
+  searchAdvertsByCategory(categoryId: string): Observable<Advert[]> {
+    const request: SearchAdvertsRequest = {categoryId};
+    return this.searchAdvert(request)
+  };
+
+  setCurrentCategory(categoryId: string | null) {
+    this.currentCategory.next(categoryId);
+  };
+
+  getCurrentCategory(): Observable<string | null> {
+    return this.currentCategory.asObservable();
+  };
+
+  getAllAdverts() :Observable<Advert[]> {
+   return this.searchAdvert({})
+  };
+
+  createAdvert(advertData: any): Observable<any> {
+   return this.advertApiService.createAdvert(advertData);
+  }
 }
