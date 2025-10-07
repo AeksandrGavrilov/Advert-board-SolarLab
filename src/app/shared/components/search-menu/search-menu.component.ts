cb9790d109faf70ace2f2a0819726e34caedcac0
AeksandrGavrilov <GavrilovAleksandr20011994@yandex.ru>
2025-10-07 16:15:14 +0400

    Внедрение меню с категориями

--- src/app/shared/components/search-menu/search-menu.component.ts
+++ src/app/shared/components/search-menu/search-menu.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import { MegaMenuModule } from 'primeng/megamenu';
+import { Component, inject } from '@angular/core';
+import { TieredMenuModule } from 'primeng/tieredmenu'; // 1. Импортируем TieredMenu
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-import { Router, RouterLink, RouterModule } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
+import { CategoryService } from '../../../services/api/category/category.service';
+import { CategoryInterface } from '../../../interfaces/category.interface';
+import { AdvertService } from '../../../services/business-logic/advert/advert.service';
+import { MenuItem } from 'primeng/api';
+import { catchError, of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-search-menu',
   imports: [
-    MegaMenuModule,
+    TieredMenuModule, 
     InputTextModule,
     ButtonModule,
     RouterLink,
@@ -18,6 +23,51 @@ import { Router, RouterLink, RouterModule } from '@angular/router';
   standalone: true,
 })
 export class SearchMenuComponent {
+  private categoryService = inject(CategoryService);
+  private advertService = inject(AdvertService)
+  menuItems: MenuItem[] = []; 
+  loading = false;
 
+  ngOnInit() {
+    this.loadCategoriesForMenu(); 
+  }
 
+  loadCategoriesForMenu() {
+    this.loading = true;
+
+    this.categoryService.getCategoriesTree().pipe(
+      tap((categoryTree: CategoryInterface[]) => {
+        this.menuItems = this.buildMenuItems(categoryTree);
+        console.log('Меню успешно создано', this.menuItems);
+      }),
+      catchError((error) => {
+      console.error('Ошибка загрузки категории', error);
+      return of(null);  
+      }),
+      tap(() => {
+        this.loading = false;
+      })
+    ).subscribe()
+  }
+
+  private buildMenuItems(categories: CategoryInterface[]): MenuItem[] {
+    return categories.map(category => {
+      const menuItem: MenuItem = {
+        label: category.name,
+        icon: 'pi pi-folder',
+        command: () => this.onCategorySelect(category.id)
+      };
+
+      if (category.items && category.items.length > 0) {
+        menuItem.items = this.buildMenuItems(category.items);
+      }
+
+      return menuItem;
+    });
+  }
+
+  private onCategorySelect(categoryId: string) {
+    console.log('Выбрана категория', categoryId);
+    this.advertService.setCurrentCategory(categoryId)
+  }
 }
