8f8501b31a235462248c92f3156aa353a9aa9644
AeksandrGavrilov <GavrilovAleksandr20011994@yandex.ru>
2025-10-08 17:32:15 +0400

    обавлена возможность загрузки изображений при создании объявления

--- src/app/shared/components/create-advert/create-advert.component.ts
+++ src/app/shared/components/create-advert/create-advert.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -6,11 +5,16 @@ import { InputTextModule } from 'primeng/inputtext';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { ButtonModule } from 'primeng/button';
 import { MessageService } from 'primeng/api';
-import { SelectModule, Select } from 'primeng/select';
+import { Select } from 'primeng/select';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { AdvertApiService } from '../../../services/api/advert-api/advert-api.service';
 import { CategoryInterface } from '../../../interfaces/category.interface';
 import { CategoryService } from '../../../services/api/category/category.service';
-import { catchError, of, tap } from 'rxjs';
+import { catchError, of, tap, Subscription, switchMap, finalize } from 'rxjs';
+import { DadataInterface } from '../../../interfaces/dadata-interface';
+import { DadataApiService } from '../../../services/api/dadata-api/dadata-api.service';
+import { ImageService } from '../../../services/business-logic/img/image.service';
 
 @Component({
   selector: 'app-create-advert',
@@ -20,7 +24,9 @@ import { catchError, of, tap } from 'rxjs';
     InputTextModule,
     InputNumberModule,
     ButtonModule,
-    Select
+    Select,
+    ProgressSpinnerModule,
+    AutoCompleteModule
 ],
   templateUrl: './create-advert.component.html',
   styleUrl: './create-advert.component.scss',
@@ -31,11 +37,18 @@ export class CreateAdvertComponent {
   private advertApiService = inject(AdvertApiService);
   private messageService = inject(MessageService);
   private categoryService = inject(CategoryService);
-  
+  private dadataApiService = inject(DadataApiService)
+  private imageService = inject(ImageService)
 
   createAdvertForm: FormGroup;
   categories: CategoryInterface[] = [];
   loadingCategories = false;
+  
+  addressSuggestion: DadataInterface[] = [];
+  loadingAddress = false;
+  
+  selectedFiles: File[] = [];
+  maxFiles = 5;
 
   constructor() {
     this.createAdvertForm = this.formBuilder.group({
@@ -74,6 +87,86 @@ export class CreateAdvertComponent {
     ).subscribe();
   }
   
+  searchAddress (event: any) {
+    console.log('🟡 Метод searchAddress вызван. event.query:', event.query);
+    const query = event.query
+
+    if (query && query.length > 2) {
+        this.loadingAddress = true;
+        this.addressSuggestion = [];
+
+        this.dadataApiService.getAdressSuggestion(query).pipe (
+            tap ((suggestion) => {
+                this.addressSuggestion = suggestion;
+                console.log('Полученны подсказки для адресов', suggestion);
+            }),
+            catchError ((error) => {
+                console.error('Не удалось загрузить адреса', error);
+                this.messageService.add({
+                    severity: 'error',
+                    summary: 'Ошибка',
+                    detail: 'Не удалось загрузить подсказки адресов'
+            });
+            return of ([]);
+        }),
+        finalize(() =>{
+            this.loadingAddress = false;
+        })
+        ).subscribe();
+    } else {
+        this.addressSuggestion = [];
+        this.loadingAddress = false;
+    }
+}
+
+    onFileSelected(event: any): void {
+        const files: FileList = event.target.files;
+        if (files) {
+            for (let i = 0; i < files.length; i++) {
+                if (files[i].type.startsWith('image/') && this.selectedFiles.length < this.maxFiles) {
+                    this.selectedFiles.push(files[i]);
+                }
+            }
+        if (this.selectedFiles.length > this.maxFiles) {
+            this.messageService.add({
+                severity: 'error',
+                summary: 'ошибка!',
+                detail: `Можно отправлять не более ${this.maxFiles} изображений` 
+            });
+        }    
+        console.log('ВЫбрано файлов', this.selectedFiles.length);
+        }
+        event.target.value = '';
+    }
+
+    removeFile(index: number): void {
+        this.selectedFiles.splice(index, 1);
+        console.log('Файл был удален, еще осталось', this.selectedFiles.length)
+    }
+
+    uploadingAllImageForAdvert( advertId: string): void {
+        if (this.selectedFiles.length === 0 ) {
+            console.log('Нет файлов для загрузки!');
+            return;
+        }
+        this.imageService.uploadSomeImages(advertId, this.selectedFiles).pipe(
+            tap((response) =>{
+                console.log('Все изображения загружены!', response);
+                this.messageService.add({
+                    severity: 'success',
+                    summary: 'Успех!',
+                    detail: `Загружено ${response.length} изображений `
+                });
+                this.selectedFiles = [];
+            }) ,
+            catchError((error) => {
+                console.log('Ошибка загрузки изображений', error);
+                return of (null);
+            })
+        )
+        .subscribe()
+    }
+
   onSubmit() {
    
     console.log('Отправка формы создания объявления');
@@ -111,6 +204,13 @@ export class CreateAdvertComponent {
                     summary: 'Успех!',
                     detail: 'Объявление создано'
                 });
+
+                const newAdvertId = response.id;
+                if (newAdvertId && this.selectedFiles.length > 0) {
+                    this.uploadingAllImageForAdvert(newAdvertId);
+                } else {
+                    this.selectedFiles = [];
+                }
                 this.createAdvertForm.reset();
             }),
             catchError((error) => {
