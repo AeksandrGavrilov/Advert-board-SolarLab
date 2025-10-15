86ccaf92b90c34e72ca1cecf135809e5dcd1ee8f
AeksandrGavrilov <GavrilovAleksandr20011994@yandex.ru>
2025-10-15 12:48:26 +0400

    внедрен компонент layout

---
+++ src/app/core/layout/layout/layout.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { SearchMenuComponent } from '../../../shared/components/search-menu/search-menu.component';
+import { BreadcrumpsComponent } from '../../../shared/components/breadcrump/breadcrump.component';
+import { ToastModule } from 'primeng/toast';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    SearchMenuComponent,
+    BreadcrumpsComponent,
+    ToastModule
+  ],
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent { }
