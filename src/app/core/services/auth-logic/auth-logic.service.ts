b676ef10b3963299f50c65c154ed4960f4248334
AeksandrGavrilov <GavrilovAleksandr20011994@yandex.ru>
2025-10-22 16:35:15 +0400

    оптимизация кода

--- src/app/core/services/auth-logic/auth-logic.service.ts
+++ src/app/core/services/auth-logic/auth-logic.service.ts
@@ -3,97 +3,115 @@ import { Router } from '@angular/router';
 import { UserInterface } from '../../interfaces/user.interface';
 import { AuthificationDataInterface } from '../../interfaces/authification-data.interface';
 import { RegisterNewUserInterface } from '../../interfaces/register-new-user.interface';
-import { BehaviorSubject, catchError, map, Observable, of, switchMap, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  map,
+  Observable,
+  of,
+  switchMap,
+  tap,
+} from 'rxjs';
 import { AuthService } from '../auth-service-api/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthLogicService {
+  private router = inject(Router);
+  private authApiService = inject(AuthService);
 
-    private router = inject(Router)
-    private authApiService = inject(AuthService)
+  private isAuthenticatedSubject = new BehaviorSubject<boolean>(
+    this.checkToken()
+  );
+  private currentUserSubject = new BehaviorSubject<UserInterface | null>(
+    this.getUserFromStorage()
+  );
 
-    private isAuthenticatedSubject  = new BehaviorSubject<boolean> (this.checkToken());
-    private currentUserSubject = new BehaviorSubject<UserInterface | null>(this.getUserFromStorage())
+  public isAuthenticated$ = this.isAuthenticatedSubject.asObservable();
+  public currentUser$ = this.currentUserSubject.asObservable();
 
-    public isAuthenticated$ = this.isAuthenticatedSubject.asObservable();
-    public currentUser$ = this.currentUserSubject.asObservable();
+  public login(authData: AuthificationDataInterface): Observable<string> {
+    return this.authApiService.login(authData).pipe(
+      tap((token: string) => {
+        const cleanToken =
+          token.startsWith('"') && token.endsWith('"')
+            ? token.slice(1, -1)
+            : token;
 
-    public login(authData: AuthificationDataInterface): Observable<string> {  
-        return this.authApiService.login(authData)
-        .pipe(
-        tap((token: string) => {
-            const cleanToken = token.startsWith('"') && token.endsWith('"') 
-                ? token.slice(1, -1) 
-                : token;
-        
-            localStorage.setItem('authToken', cleanToken)
-            localStorage.setItem('userLogin', authData.login);
+        localStorage.setItem('authToken', cleanToken);
+        localStorage.setItem('userLogin', authData.login);
 
-            const savedToken = localStorage.getItem('authToken');
-            console.log('токен сохранен в local storage, save token =',savedToken);
+        const savedToken = localStorage.getItem('authToken');
+        console.log('токен сохранен в local storage, save token =', savedToken);
 
-            this.isAuthenticatedSubject.next(true);
-            this.currentUserSubject.next({
-            id: authData.login,
-            name: authData.login,
-            login: authData.login
-            })
-        })
-        )
+        this.isAuthenticatedSubject.next(true);
+        this.currentUserSubject.next({
+          id: authData.login,
+          name: authData.login,
+          login: authData.login,
+        });
+      })
+    );
   }
 
   public register(registerData: RegisterNewUserInterface): Observable<string> {
-   
-    return this.authApiService.register(registerData)
-      .pipe(
-        switchMap(userId => {
-          console.log('Пользователь зарегистрирован с идентификатором Id:', userId);
-          return this.login({login: registerData.login, password: registerData.password}).pipe(
-            tap(() => {
-              this.currentUserSubject.next({
-                id: userId,              
-                name: registerData.name, 
-                login: registerData.login
-              }),
-              console.log('Автоматическая авторизация после регистрации завершена успешно!');
+    return this.authApiService.register(registerData).pipe(
+      switchMap((userId) => {
+        console.log(
+          'Пользователь зарегистрирован с идентификатором Id:',
+          userId
+        );
+        return this.login({
+          login: registerData.login,
+          password: registerData.password,
+        }).pipe(
+          tap(() => {
+            this.currentUserSubject.next({
+              id: userId,
+              name: registerData.name,
+              login: registerData.login,
             }),
-            catchError(error => {
-              console.error('Автоматическая авторизация - ошибка!', error);
-              return of(userId);
-            }),
-            map(() => userId) 
-          );
-        })
-      );
+              console.log(
+                'Автоматическая авторизация после регистрации завершена успешно!'
+              );
+          }),
+          catchError((error) => {
+            console.error('Автоматическая авторизация - ошибка!', error);
+            return of(userId);
+          }),
+          map(() => userId)
+        );
+      })
+    );
   }
-  
+
   public logout(): void {
-        localStorage.removeItem('authToken');
-        localStorage.removeItem('userLogin');
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('userLogin');
 
-        this.isAuthenticatedSubject.next(false);
-        this.currentUserSubject.next(null);
+    this.isAuthenticatedSubject.next(false);
+    this.currentUserSubject.next(null);
 
-        this.router.navigate(['/']);
-    }
+    this.router.navigate(['/']);
+  }
 
-    public isAuthenticated(): boolean {
-        return this.isAuthenticatedSubject.value
-    }
+  public isAuthenticated(): boolean {
+    return this.isAuthenticatedSubject.value;
+  }
 
-    private checkToken(): boolean {
-        return !!localStorage.getItem('authToken');
-    }
+  private checkToken(): boolean {
+    return !!localStorage.getItem('authToken');
+  }
 
-    private getUserFromStorage(): UserInterface | null {
-        const login = localStorage.getItem('userLogin');
-        return login ? {
-        id:login,
-        name:login,
-        login:login
-        } : null;
-    }
-
+  private getUserFromStorage(): UserInterface | null {
+    const login = localStorage.getItem('userLogin');
+    return login
+      ? {
+          id: login,
+          name: login,
+          login: login,
+        }
+      : null;
+  }
 }
